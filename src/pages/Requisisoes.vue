c00f8c4db408a9b3a6673a6a085040f054b61134
Icaro Abreu Johann <icaroabreujohann01@gmail.com>
2025-02-24 15:19:26 -0300

    Create Requisisoes.vue

---
+++ src/pages/Requisisoes.vue
@@ -0,0 +1,29 @@
+<!-- SCRIPT -->
+<script setup>
+
+    // Importar REF
+    import { onMounted, ref } from 'vue';
+
+    // Vetor dinâmico
+    let vetor = ref([]);
+
+    // Requisição
+    onMounted(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(requisicao => requisicao.json())
+        .then(postagens => vetor = postagens);
+    });
+
+</script>
+
+<!-- HTML -->
+<template>
+    <h1>Posts</h1>
+
+    <table>
+        <tr v-for="v of vetor">
+            <td>{{ v.title }}</td>
+            <td>{{ v.body }}</td>
+        </tr>
+    </table>
+</template>
